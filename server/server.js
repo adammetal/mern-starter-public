89abf852578b7ca0daa6802e98c1efaa80115d2d
adam.metal <adam.metal@codecool.com>
2022-10-01 22:12:34 +0200

    fix server error handling

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const employeesRouter = require("./routes/employees.router");
 
-const mongoUrl = process.env.MONGO_URL;
+const { MONGO_URL, PORT = 8080 } = process.env;
 
-if (!mongoUrl) {
+if (!MONGO_URL) {
   console.error("Missing MONGO_URL environment variable");
-  process.exit(1); // exit the current program
+  process.exit(1);
 }
 
 const app = express();
@@ -17,9 +17,9 @@ app.use(express.json());
 app.use("/api/employees", employeesRouter);
 
 const main = async () => {
-  await mongoose.connect(mongoUrl);
+  await mongoose.connect(MONGO_URL);
 
-  app.listen(8080, () => {
+  app.listen(PORT, () => {
     console.log("App is listening on 8080");
     console.log("Try /employees route right now");
   });
