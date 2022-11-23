066546c27e0d1334fc7889f2f087c41be50cd473
adam.metal <adam.metal@codecool.com>
2022-11-23 15:36:56 +0100

    generalize table and form css

--- client/src/index.js
+++ client/src/index.js
@@ -10,34 +10,44 @@ import EmployeeCreator from "./Pages/EmployeeCreator";
 import EmployeeUpdater from "./Pages/EmployeeUpdater";
 
 import "./index.css";
+import TableTest from "./Pages/TableTest";
+import FormTest from "./Pages/FormTest";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Layout />,
-		errorElement: <ErrorPage />,
-		children: [
-			{
-				path: "/",
-				element: <EmployeeList />,
-			},
-			{
-				path: "/create",
-				element: <EmployeeCreator />,
-			},
-			{
-				path: "/update/:id",
-				element: <EmployeeUpdater />,
-			},
-		],
-	},
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <EmployeeList />,
+      },
+      {
+        path: "/create",
+        element: <EmployeeCreator />,
+      },
+      {
+        path: "/update/:id",
+        element: <EmployeeUpdater />,
+      },
+      {
+        path: "/table-test",
+        element: <TableTest />,
+      },
+      {
+        path: "/form-test",
+        element: <FormTest />,
+      },
+    ],
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
